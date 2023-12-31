8f7ebea63772751505fcdd9f600b19c9378e17d7
Ham-ahmed <106714601+Ham-ahmed@users.noreply.github.com>
2023-12-31 10:34:19 +0200

    Add files via upload

---
+++ 1Skins-Mx-Sline-posterX-Mod-HAhmed.sh
@@ -0,0 +1,16 @@
+
+#!/bin/sh
+#
+
+wget -O /var/volatile/tmp/enigma2-plugin-skins-mx--sline-posterx_2.0-mod2ha_all.ipk "https://raw.githubusercontent.com/Ham-ahmed/Skin36/main/enigma2-plugin-skins-mx--sline-posterx_2.0-mod2ha_all.ipk"
+wait
+opkg install --force-overwrite /tmp/*.ipk
+wait
+rm -f /var/volatile/tmp/enigma2-plugin-skins-mx--sline-posterx_2.0-mod2ha_all.ipk
+wait
+sleep 2;
+exit 0
+
+
+
+
