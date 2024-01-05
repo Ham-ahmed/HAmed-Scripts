1296aff02fed2ac180a0dca259811770f403a033
Ham-ahmed <106714601+Ham-ahmed@users.noreply.github.com>
2024-01-05 07:27:29 +0200

    Add files via upload

---
+++ enigma2-plugin-extensions-novalerstore-python3_2.0-r0_all.sh
@@ -0,0 +1,16 @@
+
+#!/bin/sh
+#
+
+wget -O /var/volatile/tmp/senigma2-plugin-extensions-novalerstore-python3_2.0-r0_all.ipk "https://github.com/Ham-ahmed/HAmed-Scripts/blob/main/enigma2-plugin-extensions-novalerstore-python3_2.0-r0_all.ipk"
+wait
+opkg install --force-overwrite /tmp/*.ipk
+wait
+rm -f /var/volatile/tmp/enigma2-plugin-extensions-novalerstore-python3_2.0-r0_all.ipk
+wait
+sleep 2;
+exit 0
+
+
+
+
