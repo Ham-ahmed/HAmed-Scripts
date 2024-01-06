50c282800dd0ce59f82ed2d1603f3ae16533f3f3
Ham-ahmed <106714601+Ham-ahmed@users.noreply.github.com>
2024-01-06 15:24:23 +0200

    Add files via upload

---
+++ Added-information.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+echo ">>>Hamdy Ahmed-Panel<<<"
+echo " "
+echo " "
+sleep 1s
+echo "06-01-2023
+ ugrade"
+
+echo "> novalerstore...has been added "
+> subssupport1.5.8r9..........updated "
+> permanentclockt......has been added "
+> service_app..........has been added "
+> NewVirtualKeyBoard_sub..has been added "
+sleep 4s
+
+
+echo ">>> Upload HAhmed <<<"
+exit 0
